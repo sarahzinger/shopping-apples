9ad10e0dbc11e7fc90e17bd4e2646b779a5f3dc4
smuenzinger <sarah.muenzinger@gmail.com>
2014-05-08 15:42:52 -0400

    Clears field

--- app.js
+++ app.js
@@ -1,22 +1,23 @@
-//add an item
+//add an item, and then clears form
 $(document).ready(function(){
 	$(document).keydown(function(event){
     if (event.which == 13) {
     event.preventDefault();
 	var newAppleText = $("#newItem").val();
-	$("ul").append("<li>" + newAppleText +"<img src='applecore.png'></li>");
+	$("ul").append("<li>" + newAppleText +"<img src='applecore.png' class='applecore'></li>");
+	$("input").val("");
 }
 
 });
-
 //cross out stuff
 	$(document).on('click', 'li', function() {
 		$(this).toggleClass('cross');
 	});
 //remove an item by clicking on applecore...not working for some reason.
-$(document).on("click","img",function(){
-	$(this).parent.remove();
+$(document).on("click",".applecore",function(){
+	$(this).parent().remove();
 });
+//
 
 
 });
